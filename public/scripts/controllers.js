a7084d91c7aac2fe5f6b14dc1fac40ca1ed932e5
Pamela <pam.cdm@gmai.com>
2013-08-24 16:18:39 -0300

    set your name and show the players

--- public/scripts/controllers.js
+++ public/scripts/controllers.js
@@ -1,26 +1,29 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp').
   controller('HomeController', ['$scope','$http','$location',function($scope,$http,$location){
-		$scope.joinGame = function(){
-			$http.post('/players',$scope.name).
-				success( function (){
-					$location.path("list");
-				}).
-				error( function(data,status){
-					$scope.error = "An error ocurred. You couldn't join the game :(";
-				});
+	$scope.actions ={}; 
+	
+	
+		$scope.actions.joinGame = function(){
+		promise = 	$http.post('http://localhost:3000/player?name='+ $scope.name );
+		
+		promise.then( 
+		    function (){
+				$location.path("list");
+			}, 	
+			function(data,status){
+				$scope.error = "An error ocurred. You couldn't join the game :("
+			});
+			
 		};
   }]).
   controller('ListController',['$scope','$http',function($scope,$http){
-	$http.get('/players').
-		success( function (players){
+	$http.get('http://localhost:3000/player/all').
+		then( function (players){
 			$scope.players = players;
-		}).
-		error( function(data,status){
-			$scope.error = "We couldn't find players :(";
-		});
-
+		}, 	function(data,status){
+				$scope.error = "We couldn't find players :(";
+			});
     	
-	
 
   }])
