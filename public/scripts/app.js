c1ac6d76bdb3dbd3317b2959d98da276d3c4a93d
Thiago Felix <felixthi@gmail.com>
2013-08-24 14:31:30 -0300

    setup angular controllers

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,16 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp',['ngRoute'])
     .config(['$routeProvider', function ($routeProvider) {
-        $routeProvider.when('/', {templateUrl: 'partials/home.html'});
-        $routeProvider.when('/list', {templateUrl: 'partials/list.html'});
-        $routeProvider.otherwise({redirectTo: '/'});
+
+      $routeProvider.when('/', {
+      	templateUrl: 'partials/home.html',
+      	controller: 'HomeController'
+      });
+
+      $routeProvider.when('/list',
+      	{
+      		templateUrl: 'partials/list.html',
+      		controller: 'ListController'
+      	});
+      $routeProvider.otherwise({redirectTo: '/'});
 }]);
